8d702f3671ccae2734adecf58ff5a7ede73c74bb
rafael.almeida3@eaportal.org <rafael_almeida01@educadventista.org>
2023-11-29 11:06:16 -0300

    Instalação do handlebars e do nodemon

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express")
+const exphbs = require("express-handlebars")
 
 const app = express()
+
+app.engine('handlebars', exphbs.engine())
+app.set('view engine', 'handlebars')
 
 app.get('/', (requisicao, resposta) => {
     resposta.send("olá, mundo!")
 })
-
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
 })
