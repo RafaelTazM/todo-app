259bda0186773a0bb4165e16f84979beaef5fa45
rafael.almeida3@eaportal.org <rafael_almeida01@educadventista.org>
2023-11-29 12:02:32 -0300

    Adição dos icones de lua e de sol para troca de tema

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
-
 const app = express()
-
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
 
+app.use(express.static('public'))
+
 app.get('/', (requisicao, resposta) => {
-    resposta.send("olá, mundo!")
+    resposta.render('home')
 })
 app.listen(3000, () => {
     console.log("Servidor rodando na porta 3000!")
