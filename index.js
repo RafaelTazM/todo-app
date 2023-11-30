a8402c149fec21dc4c891cc2ecb2adbccf315587
rafael.almeida3@eaportal.org <rafael_almeida01@educadventista.org>
2023-11-30 10:03:19 -0300

    conexão com mysql finalizada

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const exphbs = require("express-handlebars")
+const mysql = require("mysql2")
 const app = express()
 app.engine('handlebars', exphbs.engine())
 app.set('view engine', 'handlebars')
@@ -9,6 +10,22 @@ app.use(express.static('public'))
 app.get('/', (requisicao, resposta) => {
     resposta.render('home')
 })
-app.listen(3000, () => {
-    console.log("Servidor rodando na porta 3000!")
+
+const conexao = mysql.createConnection({
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "todoapp",
+    port: 3306
 })
+
+conexao.connect((erro) => {
+    if(erro) {
+        return console.log(erro)
+    }
+
+    console.log("Estou conectado ao mysql!")
+
+    app.listen(3000, () => {
+        console.log("Servidor rodando na porta 3000!")
+    })})
